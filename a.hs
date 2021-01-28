21d3d4ebef4c1c0bec3b520e4115e3230e1d076e
horacio <ernesto@redqueen.local>
2021-01-28 10:31:51 -0600

    Aceptado

--- a.hs
+++ a.hs
@@ -1,13 +1,25 @@
 import Control.Monad
+import Data.List (concatMap, group)
+
+anadir:: Char -> Int -> String
+anadir '\0' _ = ""
+anadir c 0 = ""
+anadir c 1 = [c]
+anadir c n = [c]++(show n)
 
 compress':: String ->Char->Int -> String -> String
-compress' "" c n res = res++[c]++(show n)
+compress' "" c n res = res++(anadir c n)
 compress' (pc:px) c n res
-	| c /= pc = compress' px pc 1 (res++[c]++(show n))
-	| otherwise = compress' px pc (n+1) res
+        | c /= pc = compress' px pc 1 (res++(anadir c n))
+        | otherwise = compress' px pc (n+1) res
+
+compress:: String -> String
+compress p = ((concatMap format).group) p
+        where
+                format [c] = [c]
+                format p' = (head p'):(show.length) p'
 
 main :: IO ()
 main = do
   p<- getLine
-  putStrLn $ compress' p '\0' 0 ""
-
+  putStrLn $ compress p
